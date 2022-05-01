db17899c8139f9baec1e02b76d4cff1c361acfe4
Rosa-Ramirez <rramireze5@miumg.edu.gt>
2022-04-30 23:34:03 -0600

    Commit inicial

---
+++ Parcial2Progra/src/parcial2progra/Parcial2Progra.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package parcial2progra;
+
+/**
+ *
+ * @author ramir
+ */
+public class Parcial2Progra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
